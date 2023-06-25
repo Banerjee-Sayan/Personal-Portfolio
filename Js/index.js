7b914a650a89fc0b1ddd179d282eb0956ea92643
Sayan Banerjee <111704272+Banerjee-Sayan@users.noreply.github.com>
2023-06-26 01:09:28 +0530

    Add files via upload

---
+++ Js/index.js
@@ -0,0 +1,10 @@
+<script src=
+"https://cdn.jsdelivr.net/npm/typed.js@2.0.11">
+    </script>
+var typed = new Typed(".geeks", {
+    strings : ["Web Developer!!!",
+    "Data Analyst","Student","Core Developer"], 
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
